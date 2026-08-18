71626b0e90dca623ab61a93b781a92e15e2bf4fa
Nick Howes <n.howes@warwick.ac.uk>
2026-08-18 15:17:22 +0100

    UTL-421 Update build task.
    Should build as an 'app' rather than a 'lib' so it has an index.html, which Netlify looks for to generate a preview site.

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,49 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [preact()],
-  resolve: {
-    alias: {
-      react: 'preact/compat',
-      'react-dom/test-utils': 'preact/test-utils',
-      'react-dom': 'preact/compat',
+export default defineConfig(({ mode }) => {
+  const isDist = mode === 'dist';
+  
+  return {
+    plugins: [preact()],
+    resolve: {
+      alias: {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      },
     },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./src/setupTests.js'],
-  },
-  build: {
-    outDir: mode === 'dist' ? 'dist' : 'build',
-    emptyOutDir: true,
-    lib: {
-      entry: 'src/index.tsx',
-      name: 'StatusPageEmbed',
-      formats: ['iife'],
-      fileName: () => 'main.js',
-      cssFileName: 'main',
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['./src/setupTests.js'],
     },
-  },
-}));
+    build: {
+      outDir: isDist ? 'dist' : 'build',
+      emptyOutDir: true,
+      ...(isDist && {
+        lib: {
+          entry: 'src/index.tsx',
+          name: 'StatusPageEmbed',
+          formats: ['iife'],
+          fileName: () => 'main.js',
+          cssFileName: 'main',
+        },
+      }),
+      ...(!isDist && {
+        assetsDir: 'static',
+        rollupOptions: {
+          output: {
+            entryFileNames: 'js/main.js',
+            chunkFileNames: 'js/[name].js',
+            assetFileNames: ({ name }) => {
+              if (name?.endsWith('.css')) return 'css/main.css';
+              if (name?.endsWith('.map')) return 'css/[name]';
+              return 'assets/[name]';
+            },
+          },
+        },
+      }),
+    },
+  };
+});
